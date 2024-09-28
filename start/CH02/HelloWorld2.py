c6039d1b1987d75e365ac29b4803fdd7e8e2caa5
worezh <kuroneiix7@gmail.com>
2024-09-27 19:57:34 -0700

    Modified HelloWorld script

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,9 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+
+userName = input("What is your name? ")
+print(f"Hello {userName}!\n")
+
+userAge = int(input("What is your age? "))
+print(f"In 2 years, you will be {userAge + 2} years old!\n")
