4a12afc58bc38ece05cf2a01cbbc063600e03107
worezh <kuroneiix7@gmail.com>
2024-10-07 09:13:20 -0700

    Add Loop to ping

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -2,21 +2,23 @@
 # First example of pinging from Python
 # By Sak 9/30
 
-# command = "ping -c 1 -w 2 127.0.0.1 > /dev/null 2>&1"
-# Linux command
-
 import os
 import platform
+# Get current OS
 currentOS = platform.system().lower()
 
-ipAddress = "127.0.0.1"
-if currentOS == "windows":
-  command = f"ping -n 1 -w 1000 {ipAddress} > NUL" # Windows
-else:
-  command = f"ping -c 1 -w 1000 {ipAddress} > /dev/null 2>&1" # Linux
+for otect4 in range(10):
+  ipAddress = f"192.168.0." + str(otect4 + 1)
 
-res = os.system(command)
-if res == 0:
-  print(f"Ping to {ipAddress} Success")
-else:
-  print(f"Ping to {ipAddress} Failure")
+  # Build ping command based on OS
+  if currentOS == "windows":
+    command = f"ping -n 1 -w 1000 {ipAddress} > NUL" # Windows
+  else:
+    command = f"ping -c 1 -w 1000 {ipAddress} > /dev/null 2>&1" # Linux
+
+  # Run ping command and get exit code
+  res = os.system(command)
+  if res == 0:
+    print(f"Ping to {ipAddress} Success")
+  else:
+    print(f"Ping to {ipAddress} Failure")
