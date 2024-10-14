6336cdf1a2c30d5c5dc1ee4beb69745ba049eb6b
worezh <kuroneiix7@gmail.com>
2024-10-14 09:50:48 -0700

    Basic Rot13 Cryptography

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Sak
+
+# Ask user for message
+initial_message = input("What is the message? ")
+final_message = ""
+
+# Convert message to lower case
+initial_message = initial_message.lower()
+
+# Loop each letter in message
+for letter in initial_message:
+  # Convert letter to number
+  ascii_num = ord(letter)
+
+  # Check if the character is a letter
+  if ascii_num >= 97 and ascii_num <= 122:
+    # Add 13 to number
+    ascii_num = ascii_num + 13
+    
+    # IS this past "z"
+    if ascii_num > 122:
+      # subtract 26
+      ascii_num = ascii_num - 26
+  
+  # Convert back to letter
+  new_letter = chr(ascii_num)
+  final_message += new_letter
+
+# Print final message
+print(final_message)
+
+# # Encrypts/decrypts text using ROT13.
+# def rot13(text):
+#   output = ""
+#   for char in text:
+#     ascii_num = ord(char) + 13
+#     output += chr(ascii_num) if ascii_num <= 122 else chr(ascii_num - 26)
+
+# initial_message = input("What is the message? ")
+# encrypted_message = rot13(initial_message)
+# print(encrypted_message)
+
+# for letter in user_input:
+#   if letter.isupper():
+#     print(chr((ord(letter) - 65 + 13) % 26 + 65), end="")
+#   elif letter.islower():
+#     print(chr((ord(letter) - 97 + 13) % 26 + 97), end="")
+#   else:
+#     print(letter, end="")
