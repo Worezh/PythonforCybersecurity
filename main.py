3d968219c382356f42b45e40defa4d6b6e609a4c
worezh <kuroneiix7@gmail.com>
2024-11-09 12:53:32 -0800

    test

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ import codecs
 import re
 import configparser
 
-config = configparser.ConfigParser()
-config['APIKeys'] = {'GitHub': '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'}
-with open('C:/Users/Rexh/Desktop/GRC/IT102 - Scripting for Cybersecurity/secrets.ini', 'w') as configfile:
-  config.write(configfile)
